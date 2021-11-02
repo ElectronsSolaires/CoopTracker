8e0394940077205e5926a6d5d01360bd08ca31d5
Jérémie L <93078550+JeremieLeg@users.noreply.github.com>
2021-11-02 17:41:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,6 @@ COOPNAME = "Electrons Solaires"
 COOPLAT = 48.886905793549025
 COOPLON = 2.4480045203663135
 GEOJSON_DATA = "93.geojson"
-GCP_PRIVATE_BUCKET= "esbucket2021"
-GCP_PUBLIC_BUCKET="esbucket_pub"
 EH_WhPerYear = 1465000 #inhabitant equivalent (1465 kWH / habitant / year)
 df_sites = pd.DataFrame(columns=['EPID', 'SNAME', 'LNAME', 'TYPE', 'DATEINST', 'CITY','LAT','LON'])
 df_sites = df_sites.append({'EPID': '1234', 'PREFIX':'WD', 'SNAME': 'Waldeck-Rousseau', 'LNAME': 'Ecole Waldeck-Rousseau', 'TYPE':'Ecole élémentaire', 'DATEINST': datetime(2021, 2, 10, 0,0,0), 'CITY':'Les Lilas', 'LAT':'48.88189202996699','LON':'2.41970629674966'}, ignore_index=True)
