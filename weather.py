4805cc50444cdee498a26eb5f610d136755ce70d
Jeremie <you@example.com>
2022-04-01 19:25:40 +0200

    fix UTC+2 issue

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ def get_data_weather_hist(start_date, end_date, row_site, cfg):
     for single_date in utils.daterange(start_date, end_date):
         site = cfg.df_sites.iloc[row_site]['PREFIX']
         print("Getting weither data from openweathermap for site "+ site)
-        d = round(time.mktime(single_date.timetuple()))+3600
+        d = round(time.mktime(single_date.timetuple()))+7200
         response = requests.get('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat='+str(cfg.df_sites.iloc[row_site]['LAT'])+'&lon=' + str(cfg.df_sites.iloc[row_site]['LON']) + '&dt=' + str(d) + '&units=metric&appid=' + str(cfg.APIK_OWM))
         if(response.status_code != 200):
             raise Exception("Error while getting weither data for site : " + site + ", status code : "+ str(response.status_code))
