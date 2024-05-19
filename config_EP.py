3399549e28e05cf183d0f067c6f201831e76ad4b
Jérémie L <93078550+JeremieLeg@users.noreply.github.com>
2024-05-19 17:23:56 +0200

    Update config_EP.py

--- config_EP.py
+++ config_EP.py
@@ -2,7 +2,7 @@
 ##########################################################################################
 # CoopTracker
 #  - Dashboard for multiple cooperatives in Ile de France
-#    All cooperatives are mentored by Energie Partagée: https://energie-partagee.org
+#    All cooperatives are mentored by Energie Partagée: https://energie-partagee.org 
 # Copyright © 2021-2022 Électrons solaires (https://www.electrons-solaires93.org/)
 # Contacts: Jeremie L, Jean-Marie B. (Électrons solaires, webmestre@electrons-solaires93.org)
 # License: CC-BY-SA 4.0
