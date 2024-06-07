6665d759c4d19d2056c2107340bede5f0d5d4a50
Jérémie L <93078550+JeremieLeg@users.noreply.github.com>
2024-06-07 21:54:31 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -8,14 +8,14 @@ def get_data_weather_hist(start_date, end_date, row_site, cfg):
     df_prod = pd.DataFrame()
     for single_date in utils.daterange(start_date, end_date):
         site = cfg.df_sites.iloc[row_site]['PREFIX']
-        print("Getting weither data from openweathermap for site "+ site)
+        print("Getting weather data from openweathermap for site "+ site)
         d = round(time.mktime(single_date.timetuple()))+7200
         response = requests.get('https://api.openweathermap.org/data/3.0/onecall/timemachine?lat='+str(cfg.df_sites.iloc[row_site]['LAT'])+'&lon=' + str(cfg.df_sites.iloc[row_site]['LON']) + '&dt=' + str(d) + '&units=metric&appid=' + str(cfg.APIK_OWM))
         if(response.status_code != 200):
             raise Exception("Error while getting weither data for site : " + site + ", status code : "+ str(response.status_code))
         j = json.loads(response.text)
         if single_date == start_date:
-            df_prod = pd.DataFrame(j['hourly'])
+            df_prod = pd.DataFrame(j['data'])
         else:
-            df_prod = df_prod.append(pd.DataFrame(j['hourly']))
+            df_prod = df_prod.append(pd.DataFrame(j['data']))
     return df_prod
